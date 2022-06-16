0705cc78e6b494f37c7825ac00eef29fd37b4d2f
Александр <titanesty@gmail.com>
2022-06-16 23:30:58 +0300

    'g'

--- consts.js
+++ consts.js
@@ -1,12 +1,17 @@
-const element = document.querySelector('.todolist__button-add')
-const newElems = document.querySelector('.todolist__list')
-const TODO_INPUT = document.querySelector('.todolist__input')
-const del = document.querySelector('.todolist__button-del')
 const ITEM_CLASS = 'todolist__item'
 const TASC_CLASS = 'todolist__task-new'
-const BTN_CLASS = 'todolist__button-add'
+const BTN_CLASS = 'todolist__button'
 const MATERIAL_CLASS = 'material-symbols-outlined'
-const TODO_CLASS = '.todolist__input'
-const ATRIBUTE_ITEM = 'todo-index'
+const ATTRIBUTE_ITEM = 'todo-index'
+const TODO_CLASS = 'todolist__input'
 
-export {element, newElems, TODO_INPUT, del, ITEM_CLASS, TASC_CLASS, BTN_CLASS, MATERIAL_CLASS, TODO_CLASS, ATRIBUTE_ITEM}
+
+const BTN = document.querySelector(`.${BTN_CLASS}`)
+const LIST = document.querySelector('.todolist__list')
+const INPUT = document.querySelector(`.${TODO_CLASS}`)
+const BTN_DEL = document.querySelector('.todolist__button-del')
+const ERROR = document.querySelector('.error')
+const CHECKBOX = document.querySelector('.todolist__state')
+const NEW_TASK = document.querySelector(`.${TASC_CLASS}`)
+
+export {BTN, LIST, INPUT, BTN_DEL, ITEM_CLASS, TASC_CLASS, BTN_CLASS, MATERIAL_CLASS, TODO_CLASS, ATTRIBUTE_ITEM, ERROR, CHECKBOX, NEW_TASK}
--- index.js
+++ index.js
@@ -1,118 +1,145 @@
-import {element, newElems, TODO_INPUT, del, ITEM_CLASS, TASC_CLASS, BTN_CLASS, MATERIAL_CLASS, TODO_CLASS, ATRIBUTE_ITEM } from './consts.js'
-const errorText = document.querySelector('.error')
-const CHECKBOX = document.querySelector('.todolist__state')
-const NEW_TASK = document.querySelector('.todolist__task-new')
-element.addEventListener('click', addList)
+import {
+    BTN,
+    LIST,
+    INPUT,
+    BTN_DEL,
+    ITEM_CLASS,
+    TASC_CLASS,
+    BTN_CLASS,
+    MATERIAL_CLASS,
+    TODO_CLASS,
+    ATTRIBUTE_ITEM,
+    ERROR,
+    CHECKBOX,
+    NEW_TASK
+} from './consts.js'
 
-
-TODO_INPUT.addEventListener('keydown', function (e) {
-  if (e.keyCode === 13) {
-    addList()
-  }
+document.addEventListener("DOMContentLoaded", ()=>{
+    JSON.parse(localStorage.getItem('todolist')).forEach(e=>add(e))
 })
 
+const handler = function () {
+  return {
+    get (target, property) {
+      if (['[object Object]', '[object Array]'].indexOf(Object.prototype.toString.call(target[property])) > -1) {
+        return new Proxy(target[property], handler());
+      }
+      return target[property]
+    },
 
-function addList() {
-  let title = document.querySelector(TODO_CLASS).value
-  if (title && !todolist.some(i => i === title)) {
-    todolist.push({title, complited: false, est: ''})
-    document.querySelector(TODO_CLASS).value = ''
-    add()
-    validate(true)
-  } else {
-    validate(false)
-  }
-} 
-
-function validate(error) {
-  if (error) {
-    errorText.classList.remove('error--show')
-  } else {
-    errorText.classList.add('error--show')
+    set(target, property, value) {
+      target[property] = value
+      localStorage.setItem('todolist', JSON.stringify(todolist))
+      return true
+    }
   }
 }
 
-function add() {
-  const lastValue = todolist[todolist.length - 1]
+let todolist = new Proxy(JSON.parse(localStorage.getItem('todolist')) || [], handler())
 
-  const state = document.createElement('INPUT')
-  const item = document.createElement('div')
-  const task = document.createElement('input')
-  const edit = document.createElement('div')
-  const del = document.createElement('div')
+BTN.addEventListener('click', toDoHandler)
 
-  item.classList.add(ITEM_CLASS)
-  task.classList.add(TASC_CLASS)
-  edit.classList.add(BTN_CLASS, MATERIAL_CLASS, 'edit')
-  del.classList.add(BTN_CLASS, MATERIAL_CLASS)
-  state.classList.add('todolist__state')
+INPUT.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+        toDoHandler()
+    }
+})
 
-  task.setAttribute('readonly', 'readonly')
-  state.setAttribute('type', 'checkbox')
+function toDoHandler() {
+    let valueInput = INPUT.value
+    console.log(valueInput)
+    if (valueInput && !todolist.some(i => i === valueInput)) {
+        todolist.push({title: valueInput, completed: false, est: ''})
+        INPUT.value = ''
+        console.log(valueInput)
+        add()
+        validate(true)
+    } else {
+        validate(false)
+    }
+}
 
-  edit.addEventListener('click', editTask)
+function validate(error) {
+    if (error) {
+        ERROR.classList.remove('error--show')
+    } else {
+        ERROR.classList.add('error--show')
+    }
+}
 
-  function editTask(e) {
+function add(e) {
+
+    const lastIndex = todolist.length - 1
+    const lastValue = e || todolist[lastIndex]
+
+    const item = document.createElement('div')
+    const state = document.createElement('INPUT')
+    const task = document.createElement('input')
+    const edit = document.createElement('div')
+    const del = document.createElement('div')
+
+    item.classList.add(ITEM_CLASS)
+    task.classList.add(TASC_CLASS)
+    edit.classList.add(BTN_CLASS, MATERIAL_CLASS, 'edit')
+    del.classList.add(BTN_CLASS, MATERIAL_CLASS)
+    state.classList.add('todolist__state')
+
+    task.setAttribute('readonly', 'readonly')
+    state.setAttribute('type', 'checkbox')
+
+    edit.addEventListener('click', editTask)
+
+
+    item.setAttribute(ATTRIBUTE_ITEM, lastIndex)
+
+    console.log(lastValue)
+
+    task.value = lastValue.title
+    edit.innerHTML = 'edit'
+    del.innerHTML = 'delete'
+
+    LIST.append(item)
+    item.append(state, task, edit, del)
+
+    del.addEventListener('click', remove)
+    state.addEventListener('change', getCompleted)
+}
+
+function getCompleted(e) {
+    const editLocal = e.target
+    const item = editLocal.parentNode.querySelector(`.${TASC_CLASS}`)
+
+    if (editLocal.checked) {
+        todolist.completed = true
+        item.classList.add('state--ready')
+    } else {
+        item.classList.remove('state--ready')
+    }
+}
+
+function editTask(e) {
     const editLocal = e.target
     const removedItem = editLocal.parentNode
     const newTask = removedItem.querySelector('.todolist__task-new')
 
     if (editLocal.classList.contains('success')) {
-      const index = removedItem.getAttribute(ATRIBUTE_ITEM)
-      todolist[index].title = newTask.value
-      newTask.setAttribute("readonly", "readonly")
-      editLocal.classList.remove('success')
-      edit.innerHTML = 'edit'
-      return true
+        const index = removedItem.getAttribute(ATTRIBUTE_ITEM)
+        todolist[index].title = newTask.value
+        newTask.setAttribute("readonly", "readonly")
+        editLocal.classList.remove('success')
+        editLocal.innerHTML = 'edit'
+        return true
     }
-    edit.innerHTML = 'done'
+    editLocal.innerHTML = 'done'
     editLocal.classList.add('success')
-    
+
     newTask.removeAttribute("readonly")
-  }
-  
-  item.setAttribute(ATRIBUTE_ITEM, todolist.length - 1)
-
-  task.value = lastValue.title
-  edit.innerHTML = 'edit'
-  del.innerHTML = 'delete'
-
-  newElems.append(item)
-  item.append(state, task, edit, del)
-
-  del.addEventListener('click', remove)
-  state.addEventListener('change', getCompleted)
-}
-
-function getCompleted(e){
-  const editLocal = e.target
-  const item = editLocal.parentNode
-  
-  if (editLocal.checked){
-    todolist.complited = true
-    document.querySelector('.todolist__task-new').classList.add('state--ready')
-  } else {
-    document.querySelector('.todolist__task-new').classList.remove('state--ready')
-  }
 }
 
 function remove(e) {
-  const removedItem = e.target.parentNode
-  const index = removedItem.getAttribute(ATRIBUTE_ITEM)
-  todolist.splice(index, 1)
-  removedItem.remove()
-  localStorage.removeItem(task)
+    const removedItem = e.target.parentNode
+    const index = removedItem.getAttribute(ATTRIBUTE_ITEM)
+    todolist.splice(index, 1)
+    removedItem.remove()
 }
 
-let todolist = new Proxy( JSON.parse(localStorage.getItem('todolist')) || [], {
-        set (target, property, value) {
-            console.log(`Setting value ${property} to ${value}`);
-            target[property] = value;
-            localStorage.setItem('todolist', JSON.stringify(target))
-            return true;
-        },
-    get: function(target, property) {
-    console.log('getting ' + property + ' for ' + target);
-    return target[property];
-  },
-    }); 
